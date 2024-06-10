f85dab1ef4810b8b6bfe4387aa557ac9b626b17f
GIFTGISHIREN <131330747+GIFTGISHIREN@users.noreply.github.com>
2024-06-10 11:40:11 +0530

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,7 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Uug_7vMuuhXA8R9ShERCOTeYv-sHj2dJCt1UOEAjaG4/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1HdCwl1uDOZF9QGZj24GFxAvgt3p3ypvp3Ee4K5plGJk/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+
+
